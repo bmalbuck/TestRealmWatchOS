fba7b94758e15d75db14910f67c6d83d99b9d0cd
Brian Buck <bbuck911@gmail.com>
2019-08-11 08:39:14 -0700

    initial add

---
+++ TestRealmWatchOS WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  TestRealmWatchOS WatchKit Extension
+//
+//  Created by Brian Buck on 8/11/19.
+//  Copyright © 2019 Brian Buck. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
